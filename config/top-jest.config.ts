d48031665163259a3cb7e17a2c0736a0a69031cc
Bob Kerns <rwk@acm.org>
2019-10-17 21:24:44 -0700

    Fix up paths and filters so they won't be faked out by higher-level paths, like '.../build/...' in travis.

--- config/top-jest.config.ts
+++ config/top-jest.config.ts
@@ -3,13 +3,16 @@
  */
 
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-      testMatch: [
-          "*.ts",
-    "**/__tests__/*.{ts,tsx,js,jsx,mjs}",
-      "!**/*.d.ts?(x)",
-      "!**/suite-*.*",
-      "!**/build/**"
-  ]
+    preset: 'ts-jest',
+    testMatch: [
+        "<rootDir>/**/__tests__/**.{ts,tsx,js,jsx,mjs}",
+        "!**/*.d.ts?(x)",
+        "!**/suite-*.ts",
+        "!**/index*.*",
+        // <rootDir> is not substituted unless at beginning, e.g. not in negated patterns!
+        // This doesn't matter for terminal patterns, but for intermediate directories, we
+        // need to use /src/ to ensure we only match below a /src/ directory (presumably ours).
+        "!/src/**/{node_modules,lib,build,docs,tmp}/**"
+    ],
+    rootDir: "src"
 };
